dfb93604dc41cb557c901846789684f4bb74fbfe
golden-on <70214821+golden-on@users.noreply.github.com>
2024-07-07 21:39:35 +0100

    add filter Button

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    :class="{
+      'bg-blue-500 text-white': currentStatus === statusValue,
+      'bg-gray-300 text-gray-700': currentStatus !== statusValue,
+      'rounded-l-md': roundedLeft,
+      'rounded-r-md': roundedRight
+    }"
+    class="border p-2"
+    @click="$emit('update:filter', statusValue)"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    currentStatus: String,
+    statusValue: String,
+    roundedLeft: Boolean,
+    roundedRight: Boolean
+  }
+}
+</script>
