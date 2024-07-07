57452d4680f6c71e224657598954706ae40fa8d8
golden-on <70214821+golden-on@users.noreply.github.com>
2024-07-07 08:37:21 +0100

    add tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
 import router from './router'
-import { key, store } from './store'
+import store from './store'
 
-createApp(App).use(router).use(store, key).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
