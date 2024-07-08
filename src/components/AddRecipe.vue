6ed7fce6c65fb4696a3ba3d04f72963930d839e3
golden-on <70214821+golden-on@users.noreply.github.com>
2024-07-08 03:02:41 +0100

    update addRecipe

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -1,14 +1,75 @@
 <template>
-  <div>
-    <h1>Ajouter une Nouvelle Recette</h1>
-    <form @submit.prevent="addRecipe">
-      <label>Nom : <input v-model="name" required /></label>
-      <label>Description : <textarea v-model="description" required></textarea></label>
-      <label>Ingrédients : <input v-model="ingredients" required /></label>
-      <label>Instructions : <textarea v-model="instructions" required></textarea></label>
-      <label>Image : <input type="file" @change="handleFileUpload" /></label>
-      <button type="submit">Ajouter</button>
-    </form>
+  <div class="flex justify-center items-center">
+    <router-link to="/" class="block text-blue-500 hover:underline absolute top-0 left-0 m-4">
+      <button class="bg-blue-500 text-white px-4 py-2 rounded">Back</button>
+    </router-link>
+    <div class="bg-white p-8 rounded shadow">
+      <h1 class="text-2xl font-bold mb-4">Ajouter une Nouvelle Recette</h1>
+      <form @submit.prevent="addRecipe" class="space-y-4">
+        <label class="flex flex-col">
+          Nom :
+          <input
+            v-model="name"
+            required
+            class="border border-gray-300 px-2 py-1 rounded focus:border-blue-500"
+          />
+        </label>
+        <label class="flex flex-col">
+          Description :
+          <textarea
+            v-model="description"
+            required
+            class="border border-gray-300 px-2 py-1 rounded focus:border-blue-500"
+          ></textarea>
+        </label>
+        <label class="flex flex-col">
+          Ingrédients :
+          <div
+            class="border border-gray-300 px-2 py-1 rounded focus:border-blue-500 h-40 overflow-y-auto"
+          >
+            <div v-for="ingredient in ingredients" :key="ingredient">
+              <label class="flex items-center">
+                <input
+                  type="checkbox"
+                  v-model="selectedIngredients"
+                  :value="ingredient"
+                  class="mr-2"
+                />
+                {{ ingredient }}
+              </label>
+            </div>
+          </div>
+        </label>
+        <label class="flex flex-col">
+          Instructions :
+          <div>
+            <div v-for="(step, index) in instructions" :key="index">
+              <input
+                v-model="instructions[index]"
+                required
+                class="border border-gray-300 px-2 py-1 rounded focus:border-blue-500 mb-2"
+              />
+            </div>
+            <button
+              type="button"
+              @click="addStep"
+              class="bg-blue-500 text-white px-4 py-2 rounded mt-2"
+            >
+              Add Step
+            </button>
+          </div>
+        </label>
+        <label class="flex flex-col">
+          Image :
+          <input
+            type="file"
+            @change="handleFileUpload"
+            class="border border-gray-300 px-2 py-1 rounded focus:border-blue-500"
+          />
+        </label>
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Ajouter</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -24,12 +85,33 @@ export default defineComponent({
 
     const name = ref('')
     const description = ref('')
-    const ingredients = ref('')
-    const instructions = ref('')
+    const instructions = ref<string[]>([])
+    const selectedIngredients = ref([])
     const image = ref<File | null>(null)
     const imageBase64 = ref<string | null>(null)
+    const ingredients = ref([
+      'Tomatoes',
+      'Cheese',
+      'Pepperoni',
+      'Olives',
+      'Mushrooms',
+      'Peppers',
+      'Onions',
+      'Sausage',
+      'Bacon',
+      'Ham',
+      'Pineapple',
+      'Spinach',
+      'Garlic',
+      'Chicken',
+      'Beef',
+      'Broccoli',
+      'Corn',
+      'Jalapenos',
+      'Anchovies',
+      'Tuna'
+    ])
 
-    // convert the image to Base64 for persistence needs
     const handleFileUpload = (event: Event) => {
       const target = event.target as HTMLInputElement
       if (target.files && target.files[0]) {
@@ -37,9 +119,14 @@ export default defineComponent({
         reader.onload = (e) => {
           imageBase64.value = e.target?.result as string
         }
-        reader.readAsDataURL(target.files[0]) // Read the file as a data URL (Base64)
+        reader.readAsDataURL(target.files[0])
         image.value = target.files[0]
       }
+    }
+
+    // Adds a new empty step to the instructions array
+    const addStep = () => {
+      instructions.value.push('')
     }
 
     const addRecipe = () => {
@@ -48,7 +135,7 @@ export default defineComponent({
           id: Date.now(),
           name: name.value,
           description: description.value,
-          ingredients: ingredients.value.split(','),
+          ingredients: ingredients.value,
           instructions: instructions.value,
           image: imageBase64.value,
           done: false
@@ -62,16 +149,14 @@ export default defineComponent({
       name,
       description,
       ingredients,
+      selectedIngredients,
       instructions,
       image,
-      imageBase64, // Make sure to return this so it's available in the template if needed
+      imageBase64,
       handleFileUpload,
+      addStep,
       addRecipe
     }
   }
 })
 </script>
-
-<style scoped>
-/* Ajoutez votre style ici */
-</style>
