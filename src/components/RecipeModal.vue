1370991434e714d0ab8f0b195ec090fc04e4d767
golden-on <70214821+golden-on@users.noreply.github.com>
2024-07-08 03:03:45 +0100

    update recipe modal

--- src/components/RecipeModal.vue
+++ src/components/RecipeModal.vue
@@ -1,18 +1,14 @@
 <template>
   <div
-    class="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center"
+    class="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center overflow-y-auto"
     v-if="isVisible"
   >
-    <div class="bg-white p-6 rounded-lg shadow-lg max-w-md w-full m-4 relative">
+    <div class="bg-white p-6 rounded-md shadow-lg max-w-md w-full m-4 relative">
       <span class="absolute top-0 right-2 text-2xl cursor-pointer" @click="close">&times;</span>
       <div class="mb-4 pt-3">
         <img :src="recipe.image" alt="Recipe Image" class="w-full h-40 rounded-lg mb-4" />
         <h2 class="text-2xl font-bold mb-2">{{ recipe.name }}</h2>
         <p class="mb-4">{{ recipe.description }}</p>
-        <h3 class="font-semibold">Ingredients:</h3>
-        <ul class="list-disc pl-5 mb-4">
-          <li v-for="ingredient in recipe.ingredients" :key="ingredient">{{ ingredient }}</li>
-        </ul>
         <h3 class="font-semibold mb-4">Instructions:</h3>
         <div class="relative border-l-2 border-gray-200">
           <div v-for="(instruction, index) in recipe.instructions" :key="index" class="mb-6 ml-4">
@@ -21,7 +17,7 @@
             >
               <div class="h-3 w-3 bg-blue-500 rounded-full"></div>
             </div>
-            <p class="text-gray-700 text-md mb-1">{{ instruction }}</p>
+            <p class="text-gray-700 text-md mb-1">Step {{ index + 1 }}: {{ instruction }}</p>
           </div>
         </div>
       </div>
